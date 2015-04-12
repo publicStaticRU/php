59c46137c18f8fac24fe86faa86794af8e8e9664
Miker <ewb.googl@gmail.com>
2015-04-12 16:13:15 +0300

    sort

---
+++ algorithms/sort.php
@@ -0,0 +1,18 @@
+function insertionSort($mas, $masCount)
+{
+    for($i = 1; $i < $masCount; $i++)
+    {
+        $rightValue = $mas[$i];
+        $leftValue = $i - 1;
+        
+        while($leftValue >= 0 && $mas[$leftValue] > $rightValue)
+        {
+            $mas[$leftValue+1] = $mas[$leftValue];
+            $leftValue--;
+        }
+        
+        $mas[++$leftValue] = $rightValue;
+    }
+    
+    return $mas;
+}
